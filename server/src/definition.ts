d1df2e0500504bc9920136d25478345a7c9a192d
Efratror <max.avontuur@hotmail.com>
2022-04-16 08:11:19 +0200

    Refactor language features to use the parser.linemap function
    Features changed: Definition, Hover and Reference.

--- server/src/definition.ts
+++ server/src/definition.ts
@@ -1,6 +1,4 @@
 import * as server from './server'
-import { parse } from 'java-ast'
-import { ParseTree } from 'antlr4ts/tree/ParseTree'
 import * as log from './scripts/syslogs'
 import * as preprocessing from './preprocessing'
 import * as pstandard from './grammer/terms/preprocessingsnippets'
@@ -10,28 +8,16 @@ import * as javaSpecific from './grammer/terms/javaspecific'
 import { ClassDeclarationContext, VariableDeclaratorIdContext, MethodDeclarationContext } from 'java-ast/dist/parser/JavaParser';
 import * as sketch from './sketch'
 
-let currentTempAST: [ParseTree][] = new Array();
-let _currentTempCounter = -1
 // [string,string,number,number] => [type, name, line number, character number]
 let foundDeclaration: [string,string,number,number][] = new Array();
 let _foundDeclarationCount = 0
 
-export function scheduleLookUpDefinition(receivedUri: string,lineNumber: number, charNumber: number): Definition | null  {
+export function scheduleLookUpDefinition(receivedUri: string, lineNumber: number, charNumber: number): Definition | null  {
 	let currentContent = server.latestChangesInTextDoc.getText()
 	let splitDefine = currentContent.split(`\n`)
 	let currentLine = splitDefine[lineNumber]
-	let currentTokens = parse(currentLine)
-	// [string, number, number] => [Token Name, Start Character, End Character]
-	let currentDefineMap: [string, number, number][] = new Array()
-	let _currentDefineCount = 0
-	for (let i = 0; i < currentTokens.childCount; i++) {
-		currentLineASTExtract(currentTokens.children![i])
-	}
-	currentTempAST.forEach(function (word) {
-		currentDefineMap[_currentDefineCount] = [word[0].text, currentLine.indexOf(word[0].text), currentLine.indexOf(word[0].text) + word[0].text.length]
-		_currentDefineCount += 1
-	})
-	
+	let currentDefineMap = parser.lineMap(currentLine)
+
 	let adjustOffset = 0
 	if(preprocessing.defaultBehaviourEnable){
 		adjustOffset = pstandard.reduceLineDefaultBehaviour
@@ -93,17 +79,7 @@ export function scheduleLookUpDefinition(receivedUri: string,lineNumber: number,
 	return finalDefinition
 }
 
-function currentLineASTExtract(gotOne: ParseTree){
-	_currentTempCounter += 1
-	currentTempAST[_currentTempCounter] = [gotOne]
-	for(let j=0;j<gotOne.childCount;j++){
-		currentLineASTExtract(gotOne.getChild(j))
-	}
-}
-
 function clearTempAST(){
-	currentTempAST = []
-	_currentTempCounter = 0
 	foundDeclaration = []
 	_foundDeclarationCount = 0
 }
