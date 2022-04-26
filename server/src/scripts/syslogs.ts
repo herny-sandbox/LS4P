4ca464a7adee696c5a6c4d5d83b4833d699ecf76
Efratror <max.avontuur@hotmail.com>
2022-04-26 14:00:41 +0200

    Replace capital String with lowercase string

--- server/src/scripts/syslogs.ts
+++ server/src/scripts/syslogs.ts
@@ -1,6 +1,6 @@
 const exec = require('child_process').exec
 
-export async function writeLog(logContents: String) {
+export async function writeLog(logContents: string) {
 	let datetime = new Date()
 	exec(`echo \"${datetime.toISOString().slice(0,22)} -> ${logContents}\" >> ${__dirname.substring(0,__dirname.length-8)}/logs/syslogs.txt`)
 }
